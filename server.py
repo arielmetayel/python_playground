c0fb188443ca3a1581c1a9c9daa36a48de232e87
Ariel <ariel@Ariels-MacBook-Pro.local>
2020-03-11 21:50:08 +0200

    commenting out

--- server.py
+++ server.py
@@ -11,10 +11,10 @@ def multiply_by_two(namber):
     return str(namber*2)
 
 
-@app.route('/guess_the_number/<int:namber>')
-def guess (namber):
-    did_you_guess = 'no'
-    return ('did_you_guess: ' + did_you_guess)
+#@app.route('/guess_the_number/<int:namber>')
+#def guess (namber):
+#    did_you_guess = 'no'
+#    return ('did_you_guess: ' + did_you_guess)
 #Guess The Number
 #Write a programme where the computer randomly generates a number between 0 and 20. The user needs to guess what the number is. If the user guesses wrong, tell them their guess is either too high, or too low. This will get you started with the random library if you haven't already used it.
 
